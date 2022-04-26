c9a8bc85704350d4ccf2978f1fd344250c4dd93c
muhangga <anggayasdoe11@gmail.com>
2022-04-26 23:24:30 +0700

    answer assignment

--- backend/concurrency/assignment/sum-of-square-number-sm-cp/main.go
+++ backend/concurrency/assignment/sum-of-square-number-sm-cp/main.go
@@ -7,6 +7,7 @@ import (
 
 var mu sync.Mutex
 var result int
+var wg sync.WaitGroup
 
 //fungsi ini digunakan untuk menerima angka lalu mengembalikan hasil pangkat 2 angka tersebut
 func squareWorker(workerInput <-chan int, workerOutput chan<- int) {
@@ -19,15 +20,26 @@ func squareWorker(workerInput <-chan int, workerOutput chan<- int) {
 func createRequest(workerInput chan<- int, workerOutput <-chan int, wg *sync.WaitGroup) {
 	for i := 1; i < 100; i++ {
 		// TODO: answer here
+		wg.Add(1)		
+
 		go func(i int) {
 			// TODO: answer here
+			defer wg.Done()
 
 			var res int
 
 			// TODO: answer here
+			workerInput <- i // kirim angka ke worker
+			res = <-workerOutput // menerima hasil dari worker
+
+			mu.Lock()
+			result += res // menjumlahkan hasil
+			mu.Unlock()
 
 			//tambahkan res ke result. Selain itu gunakan juga sesuatu yang menghindari data race
 			// TODO: answer here
+			
+
 			fmt.Println(res)
 		}(i)
 	}
